daa26c097ce292b6130367954cca931d960bbcc7
Manas Kanithahalli Sadananda <manas.ks15@gmail.com>
2015-07-16 20:17:22 -0700

    smoke.py

--- project1/tap/regression/tests/smoke.py
+++ project1/tap/regression/tests/smoke.py
@@ -13,37 +13,45 @@ import unittest
 
 class smoke(unittest.TestCase):
     """ smoke/power on tests, hopefully won't produce actual smoke """
-    
+
     def setUp(self):
         """ fires before each test
         Setting up for the test
-    
+
         """
-        log_level = LOG_LEVEL 
+        log_level = LOG_LEVEL
         self.logger = get_logger(self.id(), log_level)
         log(self.logger, 'info', '{}Running {}'.format(color_map['highlight'],self.id()))
 
         self.tap = Tap(log_level=log_level)
         self.loopback_monitor = LoopBack(log_level=log_level)
         self.loopback_monitor.set_monitor()
-    
+
     def tearDown(self):
         """ fires after each test
         Cleaning up after the test
-    
+
         """
         self.loopback_monitor.remove_monitor()
         self.tap.clean_up()
-        log(self.logger, 'info', '{}Done with {}\n'.format(color_map['highlight'],self.id()))    
-    
+        log(self.logger, 'info', '{}Done with {}\n'.format(color_map['highlight'],self.id()))  
+
     def testReset(self):
         self.tap.reset()
         self.assertEqual("Test_Logic_Reset",self.loopback_monitor.cur_state)
 
-    @unittest.skip
+    #@unittest.skip
     def testReset2ShiftIR(self):
-        pass
+        self.tap.reset()
+        self.tap.reset2ShiftIR()
+        self.assertEqual("Shift_IR",self.loopback_monitor.cur_state)
 
-    @unittest.skip
+    #@unittest.skip
     def testReadDeviceCode(self):
-        pass
+        self.tap.reset()
+        self.tap.reset2ShiftIR()
+        self.tap.shiftInData("100100")
+        self.tap.exit1IR2ShiftDR()
+        result = self.tap.shiftOutData(32)
+        self.assertEqual("0x0",result)
+        #print("IDCODE: %x"%int(result))
